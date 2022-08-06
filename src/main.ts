5dff5e0b41163343f94020573bd701559e936103
thoeunsopheara <thoeunsopheara@gmail.com>
2022-08-06 12:22:21 +0700

    setup swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fullstack Bootcamp recipes api')
+    .setDescription('This is KIT fullstack bootcamp food recipes api')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
