b671e4b59ff02c045e59ded36c658fab7de2be4a
thoeunsopheara <thoeunsopheara@gmail.com>
2022-08-07 21:49:36 +0700

    make prisma global module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { UsersModule } from "@users/users.module";
-
-
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     UsersModule,
-    ConfigModule.forRoot({ isGlobal: true })
-  ],
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule
+  ]
 })
 export class AppModule {}
