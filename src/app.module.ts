24d0c635f2eb1c366130399eb7585b842e0f8e81
thoeunsopheara <thoeunsopheara@gmail.com>
2022-08-07 17:20:42 +0700

    generate resource users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './api/users/users.module';
 
 
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
 })
 export class AppModule {}
